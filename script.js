9d1d94c4815943a3f573b5e8f66e20e9350992ab
juliogazoli <julio_gazoli@hotmail.com>
2023-09-15 12:40:31 -0300

    Síncrono ou Assíncrono?

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello world!");
+console.log("Mandando oi pro amigo!");
+
+function mandaMensagem() {
+    console.log("Tudo bem?");
+    console.log("Vou te mandar uma solicitação!");
+    console.log("Solicitação recebida!");
+}
+
+mandaMensagem();
+
+console.log("Tchau tchau!")
