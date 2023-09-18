31129841435ae917a38e55fb9135f4013c07033e
juliogazoli <julio_gazoli@hotmail.com>
2023-09-18 11:54:30 -0300

    Consulta dinâmica

--- script.js
+++ script.js
@@ -12,7 +12,5 @@ async function buscaEndereco(cep) {
     }
 };
 
-let ceps = ['01001000', '01001001'];
-let conjuntoCeps = ceps.map(valores => buscaEndereco(valores));
-console.log(conjuntoCeps);
-Promise.all(conjuntoCeps).then(respostas => console.log(respostas))
+var cep = document.getElementById('cep');
+cep.addEventListener("focusout", () => buscaEndereco(cep.value));
